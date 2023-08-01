7b5da25f84af0e7bac2926904ff48dfd84c698a4
sahithikona <konadevisahithi20@gmail.com>
2023-08-01 12:39:40 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pythonfeatures(request):
+    return render(request,"pythonfeatures.html")
---
+++ app3/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def decoraterexplanation(request):
+    return render(request,"decoraterexplanation.html")
